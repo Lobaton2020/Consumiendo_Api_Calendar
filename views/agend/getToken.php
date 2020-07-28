4bf6b0eeba03ef2374347a906968e12c0ed47bd0
Andres Felipe <andrespipe021028@gmail.com>
2020-07-28 16:37:52 -0500

    prueba

--- views/agend/getToken.php
+++ views/agend/getToken.php
@@ -1,6 +1,6 @@
 <form method="POST">
-<input type="text" name="c" value="agend" >
-<input type="text" name="m" value="validateToken" > 
-  <input type="text"  name="token" > 
+<input type="hidden" name="c" value="agend" >
+<input type="hidden" name="m" value="validateToken" > 
+  <input type="text"  name="token" placeholder="Token"> 
   <input type="submit" value="Enviar Token">
 </form>
