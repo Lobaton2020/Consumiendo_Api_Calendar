5cb21e223f52a806876eec10a785dca4f27b08de
Andres Felipe <andrespipe021028@gmail.com>
2020-07-28 16:40:38 -0500

    prueba

--- quickstart.php
+++ quickstart.php
@@ -1,6 +1,9 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
+if(isset($_GET["code"])){
+    echo $_GET["code"];
+}
 // if (php_sapi_name() != 'cli') {
 //     throw new Exception('This application must be run on the command line.');
 // }
