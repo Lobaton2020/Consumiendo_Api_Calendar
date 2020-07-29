62fe5e82865dff2246e13c8eb914e58556f792e8
Andres Felipe <andrespipe021028@gmail.com>
2020-07-28 22:13:24 -0500

    prueba

--- index.php
+++ index.php
@@ -1,40 +1,36 @@
 
 <?php
 require_once "helpers/helperController.php";
-require_once "config/config.php";
-
 
 $controller = "Agend";
 $method = "index";
 
-if(!isset($_REQUEST["c"])){
+if (!isset($_REQUEST["c"])) {
     require_once "controllers/{$controller}Controller.php";
-    $controller = ucwords($controller)."Controller";
+    $controller = ucwords($controller) . "Controller";
     $controller = new $controller();
-    call_user_func([$controller,$method]);
-}else{
+    call_user_func([$controller, $method]);
+} else {
 
-    $controller = ucwords($controller)."Controller";
+    $controller = ucwords($controller) . "Controller";
     $method = isset($_REQUEST["m"]) ? $_REQUEST["m"] : $method;
 
-    if(file_exists("controllers/{$controller}.php")){
+    if (file_exists("controllers/{$controller}.php")) {
         require_once "controllers/{$controller}.php";
-        
-        if(class_exists($controller)){
+
+        if (class_exists($controller)) {
             $controller = new $controller();
 
-            if(method_exists($controller,$method)){
-                call_user_func([$controller,$method]);
-            }else{
+            if (method_exists($controller, $method)) {
+                call_user_func([$controller, $method]);
+            } else {
                 echo "Pagina inexistente";
             }
-        }else{
+        } else {
             echo "Controlador no encontradp";
         }
-    }else{
+    } else {
         echo "El archivo no existe";
     }
-
-
 
 }
